b0a649b99b178ce833892fc2c80e313ccb28cb1c
dobeid <dialajo9@gmail.com>
2024-01-30 00:05:35 -0800

    created the first section of the function based on acceptance criteria

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,18 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
+  // listener for click events on the save button
+  $(".saveBtn").on("click", function () {
+    // time-block ID to be used as a key
+    let timeBlock = $(this).parent().attr("id");
+
+    // get user input from the textarea
+    let userInput = $(this).siblings(".description").val();
+
+    // saving user input to local storage using the blockId as a key
+    localStorage.setItem(timeBlock, userInput);
+  });
+  
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
