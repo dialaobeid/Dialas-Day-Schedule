6bda4c456143fadcf27444b7c6e7e3c6375c9381
dobeid <dialajo9@gmail.com>
2024-01-30 00:42:31 -0800

    created under input fx to be able to get user input from browser's local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,49 +2,58 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  // listener for click events on the save button
-  $(".saveBtn").on("click", function () {
-    // time-block ID to be used as a key
-    let timeBlock = $(this).parent().attr("id");
+    // listener for click events on the save button
+    $(".saveBtn").on("click", function () {
+        // time-block ID to be used as a key
+        let timeBlock = $(this).parent().attr("id");
 
-    // get user input from the textarea
-    let userInput = $(this).siblings(".description").val();
+        // get user input from the textarea
+        let userInput = $(this).siblings(".description").val();
 
-    // saving user input to local storage using the blockId as a key
-    localStorage.setItem(timeBlock, userInput);
-  });
-  
+        // saving user input to local storage using the blockId as a key
+        localStorage.setItem(timeBlock, userInput);
+    });
+
     // Function to apply past, present, or future class to each time block
     function timeBlockColors() {
         // gets current hour using Day.js
         let currentHour = dayjs().hour();
 
-    // Loop through each time block
-    $(".time-block").each(function () {
-        // gets the hour from the ID attribute of current time block
-        // parseInt then converts this into an integer 
-        let blockHour = parseInt($(this).attr("id").split("-")[1]);
-  
-        // resets the past, present, future classes
-        // then adds the appropriate class based on current hour
-        $(this).removeClass("past present future");
-  
-        // IF statement checks whether current block hour is classified as past, present or future
-        if (blockHour < currentHour) {
-          $(this).addClass("past");
-        } else if (blockHour === currentHour) {
-          $(this).addClass("present");
-        } else {
-          $(this).addClass("future");
-        }
-      });
+        // Loop through each time block
+        $(".time-block").each(function () {
+            // gets the hour from the ID attribute of current time block
+            // parseInt then converts this into an integer 
+            let blockHour = parseInt($(this).attr("id").split("-")[1]);
+
+            // resets the past, present, future classes
+            // then adds the appropriate class based on current hour
+            $(this).removeClass("past present future");
+
+            // IF statement checks whether current block hour is classified as past, present or future
+            if (blockHour < currentHour) {
+                $(this).addClass("past");
+            } else if (blockHour === currentHour) {
+                $(this).addClass("present");
+            } else {
+                $(this).addClass("future");
+            }
+        });
     }
-  
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
+    // Function to extract user input from local storage
+    function getUserInput() {
+        // Another loop through each time block
+        $(".time-block").each(function () {
+            // gets time block ID to retrieve the appropriate user input from local storage
+            let blockId = $(this).attr("id");
+            let userInput = localStorage.getItem(blockId);
+
+            // retreived user input in text
+            $(this).find(".description").val(userInput);
+        });
+    }
+    
+    // TODO: Add code to display the current date in the header of the page.
 });
 
 
